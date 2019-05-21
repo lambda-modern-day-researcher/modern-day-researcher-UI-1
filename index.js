14c79fe9e358b6d8176eca51e35c3b5c41fa1fbe
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-21 10:00:51 -0500

    fixed after an unsuccessful attempt at a carousel

--- index.js
+++ index.js
@@ -14,21 +14,20 @@ const toggleMenu = () => {
   
    class Category {
     constructor(domElement) {
-      // assign this.domElement to the passed in domElement
+      
       this.domElement = domElement;
-      // this.domElement = document.querySelector('category');
-      // create a reference to the ".expandButton" class. 
+    
       this.expandButton = domElement.querySelector('.expandButton');
-      // Using your expandButton reference, update the text on your expandButton to say "expand"
+      
       this.expandButton.textContent = ('Open');
-      // Set a click handler on the expandButton reference, calling the expandCategory method.
+      
       this.expandButton.addEventListener('click', () => {
         this.expandCategory()
       })
     }
  
     expandCategory() {
-      // Using our reference to the domElement, toggle a class to expand or hide the category.
+     
       this.domElement.classList.toggle('category-open')
       if (this.expandButton.textContent == 'Open'){
        this.expandButton.textContent = 'Close';
@@ -38,12 +37,7 @@ const toggleMenu = () => {
     }
   }
  
-//   START HERE: 
- 
-//  - Select all classes named ".article" and assign that value to the articles variable.  
- 
-//  - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
- 
+
  
   let categories = document.querySelectorAll('.category');
  categories.forEach(category => {return new Category(category)}
